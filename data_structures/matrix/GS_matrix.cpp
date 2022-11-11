9351cd019a28b6c24cab2065830195a9c2462869
GerSay <kryt.master@yandex.ru>
2022-11-11 15:35:57 +0300

    fixed

--- data_structures/matrix/GS_matrix.cpp
+++ data_structures/matrix/GS_matrix.cpp
@@ -3,20 +3,17 @@
 namespace GS {
     Matrix::Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
         allocSpace();
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] = 0;
-            }
-        }
     }
 
     Matrix::Matrix(double **a, int rows, int cols) : rows_(rows), cols_(cols) {
         allocSpace();
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] = a[i][j];
-            }
-        }
+
     }
 
     Matrix::Matrix() : rows_(1), cols_(1) {
@@ -25,30 +22,27 @@ namespace GS {
     }
 
     Matrix::~Matrix() {
-        for (int i = 0; i < rows_; ++i) {
-            delete[] p[i];
-        }
+        for (int i = 0; i < rows_; ++i) delete[] p[i];
         delete[] p;
     }
 
     Matrix::Matrix(const Matrix &m) : rows_(m.rows_), cols_(m.cols_) {
         allocSpace();
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] = m.p[i][j];
-            }
-        }
+
+
     }
 
     Matrix &Matrix::operator=(const Matrix &m) {
-        if (this == &m) {
+        if (this == &m)
             return *this;
-        }
 
         if (rows_ != m.rows_ || cols_ != m.cols_) {
-            for (int i = 0; i < rows_; ++i) {
+            for (int i = 0; i < rows_; ++i)
                 delete[] p[i];
-            }
+
             delete[] p;
 
             rows_ = m.rows_;
@@ -56,59 +50,52 @@ namespace GS {
             allocSpace();
         }
 
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] = m.p[i][j];
-            }
-        }
+
         return *this;
     }
 
     Matrix &Matrix::operator+=(const Matrix &m) {
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] += m.p[i][j];
-            }
-        }
+
         return *this;
     }
 
     Matrix &Matrix::operator-=(const Matrix &m) {
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] -= m.p[i][j];
-            }
-        }
+
         return *this;
     }
 
     Matrix &Matrix::operator*=(const Matrix &m) {
         Matrix temp(rows_, m.cols_);
-        for (int i = 0; i < temp.rows_; ++i) {
-            for (int j = 0; j < temp.cols_; ++j) {
-                for (int k = 0; k < cols_; ++k) {
+        for (int i = 0; i < temp.rows_; ++i)
+            for (int j = 0; j < temp.cols_; ++j)
+                for (int k = 0; k < cols_; ++k)
                     temp.p[i][j] += (p[i][k] * m.p[k][j]);
-                }
-            }
-        }
+
         return (*this = temp);
     }
 
     Matrix &Matrix::operator*=(double num) {
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] *= num;
-            }
-        }
+
         return *this;
     }
 
     Matrix &Matrix::operator/=(double num) {
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 p[i][j] /= num;
-            }
-        }
+
         return *this;
     }
 
@@ -125,39 +112,30 @@ namespace GS {
 
     Matrix Matrix::transpose() {
         Matrix ret(rows_, cols_);
-        for (int i = 0; i < rows_; ++i) {
-            for (int j = 0; j < cols_; ++j) {
+        for (int i = 0; i < rows_; ++i)
+            for (int j = 0; j < cols_; ++j)
                 ret.p[j][i] = p[i][j];
-            }
-        }
+
         return ret;
     }
 
-
-    /* STATIC CLASS FUNCTIONS
-     ********************************/
-
     Matrix Matrix::createIdentity(int size) {
         Matrix temp(size, size);
-        for (int i = 0; i < temp.rows_; ++i) {
-            for (int j = 0; j < temp.cols_; ++j) {
-                if (i == j) {
+        for (int i = 0; i < temp.rows_; ++i)
+            for (int j = 0; j < temp.cols_; ++j)
+                if (i == j)
                     temp.p[i][j] = 1;
-                } else {
+                else
                     temp.p[i][j] = 0;
-                }
-            }
-        }
+
         return temp;
     }
 
     Matrix Matrix::solve(const Matrix& A, const Matrix& b) {
-        // Gaussian elimination
         for (int i = 0; i < A.rows_; ++i) {
-            if (A.p[i][i] == 0) {
-                // pivot 0 - throw error
+            if (A.p[i][i] == 0)
                 throw std::domain_error("Error: the coefficient matrix has 0 as a pivot. Please fix the input and try again.");
-            }
+
             for (int j = i + 1; j < A.rows_; ++j) {
                 for (int k = i + 1; k < A.cols_; ++k) {
                     A.p[j][k] -= A.p[i][k] * (A.p[j][i] / A.p[i][i]);
@@ -171,16 +149,15 @@ namespace GS {
             }
         }
 
-        // Back substitution
         Matrix x(b.rows_, 1);
         x.p[x.rows_ - 1][0] = b.p[x.rows_ - 1][0] / A.p[x.rows_ - 1][x.rows_ - 1];
         if (x.p[x.rows_ - 1][0] < EPS && x.p[x.rows_ - 1][0] > -1 * EPS)
             x.p[x.rows_ - 1][0] = 0;
         for (int i = x.rows_ - 2; i >= 0; --i) {
-            int sum = 0;
-            for (int j = i + 1; j < x.rows_; ++j) {
+            auto sum = 0;
+            for (int j = i + 1; j < x.rows_; ++j)
                 sum += A.p[i][j] * x.p[j][0];
-            }
+
             x.p[i][0] = (b.p[i][0] - sum) / A.p[i][i];
             if (x.p[i][0] < EPS && x.p[i][0] > -1 * EPS)
                 x.p[i][0] = 0;
@@ -190,58 +167,51 @@ namespace GS {
     }
 
     Matrix Matrix::bandSolve(const Matrix& A, const Matrix& b, int32_t k) {
-        // optimized Gaussian elimination
         int bandsBelow = (k - 1) / 2;
         for (int i = 0; i < A.rows_; ++i) {
-            if (A.p[i][i] == 0) {
-                // pivot 0 - throw exception
+            if (A.p[i][i] == 0)
                 throw std::domain_error("Error: the coefficient matrix has 0 as a pivot. Please fix the input and try again.");
-            }
+
             for (int j = i + 1; j < A.rows_ && j <= i + bandsBelow; ++j) {
-                int k = i + 1;
-                while (k < A.cols_ && A.p[j][k]) {
-                    A.p[j][k] -= A.p[i][k] * (A.p[j][i] / A.p[i][i]);
-                    k++;
+                int iter = i + 1;
+                while (iter < A.cols_ && A.p[j][iter]) {
+                    A.p[j][iter] -= A.p[i][iter] * (A.p[j][i] / A.p[i][i]);
+                    iter++;
                 }
                 b.p[j][0] -= b.p[i][0] * (A.p[j][i] / A.p[i][i]);
                 A.p[j][i] = 0;
             }
         }
 
-        // Back substitution
         Matrix x(b.rows_, 1);
         x.p[x.rows_ - 1][0] = b.p[x.rows_ - 1][0] / A.p[x.rows_ - 1][x.rows_ - 1];
         for (int i = x.rows_ - 2; i >= 0; --i) {
             int sum = 0;
-            for (int j = i + 1; j < x.rows_; ++j) {
+            for (int j = i + 1; j < x.rows_; ++j)
                 sum += A.p[i][j] * x.p[j][0];
-            }
+
             x.p[i][0] = (b.p[i][0] - sum) / A.p[i][i];
         }
 
         return x;
     }
 
-    // functions on VECTORS
     double Matrix::dotProduct(Matrix a, Matrix b) {
         double sum = 0;
-        for (int i = 0; i < a.rows_; ++i) {
-            sum += (a(i, 0) * b(i, 0));
-        }
+        for (int i = 0; i < a.rows_; ++i) sum += (a(i, 0) * b(i, 0));
+
         return sum;
     }
 
-    // functions on AUGMENTED matrices
     Matrix Matrix::augment(Matrix A, Matrix B) {
         Matrix AB(A.rows_, A.cols_ + B.cols_);
-        for (int i = 0; i < AB.rows_; ++i) {
-            for (int j = 0; j < AB.cols_; ++j) {
+        for (int i = 0; i < AB.rows_; ++i)
+            for (int j = 0; j < AB.cols_; ++j)
                 if (j < A.cols_)
                     AB(i, j) = A(i, j);
                 else
                     AB(i, j) = B(i, j - B.cols_);
-            }
-        }
+
         return AB;
     }
 
@@ -249,19 +219,17 @@ namespace GS {
         Matrix Ab(*this);
         int rows = Ab.rows_;
         int cols = Ab.cols_;
-        int Acols = cols - 1;
+        int _cols = cols - 1;
 
-        int i = 0; // row tracker
-        int j = 0; // column tracker
+        int i = 0;
+        int j = 0;
 
-        // iterate through the rows
         while (i < rows) {
-            // find a pivot for the row
             bool pivot_found = false;
-            while (j < Acols && !pivot_found) {
-                if (Ab(i, j) != 0) { // pivot not equal to 0
+            while (j < _cols && !pivot_found) {
+                if (Ab(i, j) != 0) {
                     pivot_found = true;
-                } else { // check for a possible swap
+                } else {
                     int max_row = i;
                     double max_val = 0;
                     for (int k = i + 1; k < rows; ++k) {
@@ -274,14 +242,12 @@ namespace GS {
                     if (max_row != i) {
                         Ab.swapRows(max_row, i);
                         pivot_found = true;
-                    } else {
+                    } else
                         j++;
-                    }
                 }
             }
 
-            // perform elimination as normal if pivot was found
-            if (pivot_found) {
+            if (pivot_found)
                 for (int t = i + 1; t < rows; ++t) {
                     for (int s = j + 1; s < cols; ++s) {
                         Ab(t, s) = Ab(t, s) - Ab(i, s) * (Ab(t, j) / Ab(i, j));
@@ -290,7 +256,6 @@ namespace GS {
                     }
                     Ab(t, j) = 0;
                 }
-            }
 
             i++;
             j++;
@@ -304,12 +269,12 @@ namespace GS {
         int rows = R.rows_;
         int cols = R.cols_;
 
-        int i = rows - 1; // row tracker
-        int j = cols - 2; // column tracker
+        int i = rows - 1;
+        int j = cols - 2;
 
-        // iterate through every row
+
         while (i >= 0) {
-            // find the pivot column
+
             int k = j - 1;
             while (k >= 0) {
                 if (R(i, k) != 0)
@@ -317,21 +282,18 @@ namespace GS {
                 k--;
             }
 
-            // zero out elements above pivots if pivot not 0
             if (R(i, j) != 0) {
 
                 for (int t = i - 1; t >= 0; --t) {
-                    for (int s = 0; s < cols; ++s) {
+                    for (int s = 0; s < cols; ++s)
                         if (s != j) {
                             R(t, s) = R(t, s) - R(i, s) * (R(t, j) / R(i, j));
                             if (R(t, s) < EPS && R(t, s) > -1 * EPS)
                                 R(t, s) = 0;
                         }
-                    }
                     R(t, j) = 0;
                 }
 
-                // divide row by pivot
                 for (int k = j + 1; k < cols; ++k) {
                     R(i, k) = R(i, k) / R(i, j);
                     if (R(i, k) < EPS && R(i, k) > -1 * EPS)
@@ -351,16 +313,15 @@ namespace GS {
     void Matrix::readSolutionsFromRREF(std::ostream &os) {
         Matrix R(*this);
 
-        // print number of solutions
         bool hasSolutions = true;
         bool doneSearching = false;
         int i = 0;
         while (!doneSearching && i < rows_) {
             bool allZeros = true;
-            for (int j = 0; j < cols_ - 1; ++j) {
+            for (int j = 0; j < cols_ - 1; ++j)
                 if (R(i, j) != 0)
                     allZeros = false;
-            }
+
             if (allZeros && R(i, cols_ - 1) != 0) {
                 hasSolutions = false;
                 os << "NO SOLUTIONS\n\n";
@@ -378,15 +339,15 @@ namespace GS {
             Matrix particular(cols_ - 1, 1);
             Matrix special(cols_ - 1, 1);
 
-            for (int i = 0; i < rows_; ++i) {
+            for (i = 0; i < rows_; ++i) {
                 bool pivotFound = false;
                 bool specialCreated = false;
-                for (int j = 0; j < cols_ - 1; ++j) {
+                for (int j = 0; j < cols_ - 1; ++j)
                     if (R(i, j) != 0) {
                         if (!pivotFound) {
                             pivotFound = true;
                             particular(j, 0) = R(i, cols_ - 1);
-                        } else { // otherwise, add to special solution
+                        } else {
                             if (!specialCreated) {
                                 special = Matrix(cols_ - 1, 1);
                                 specialCreated = true;
@@ -394,10 +355,9 @@ namespace GS {
                             special(j, 0) = -1 * R(i, j);
                         }
                     }
-                }
-                os << "Special solution:" << std::endl << special << std::endl;
+                os << "Special solution:\n" << special << '\n';
             }
-            os << "Particular solution:" << std::endl << particular << std::endl;
+            os << "Particular solution:\n" << particular << '\n';
         }
     }
 
@@ -407,87 +367,65 @@ namespace GS {
         Matrix U = AI.gaussianEliminate();
         Matrix IAInverse = U.rowReduceFromGaussian();
         Matrix AInverse(rows_, cols_);
-        for (int i = 0; i < AInverse.rows_; ++i) {
-            for (int j = 0; j < AInverse.cols_; ++j) {
+
+        for (int i = 0; i < AInverse.rows_; ++i)
+            for (int j = 0; j < AInverse.cols_; ++j)
                 AInverse(i, j) = IAInverse(i, j + cols_);
-            }
-        }
+
         return AInverse;
     }
 
 
-    /* PRIVATE HELPER FUNCTIONS
-     ********************************/
-
     void Matrix::allocSpace() {
         p = new double *[rows_];
-        for (int i = 0; i < rows_; ++i) {
-            p[i] = new double[cols_];
-        }
+        for (int i = 0; i < rows_; ++i) p[i] = new double[cols_];
     }
 
     Matrix Matrix::expHelper(const Matrix &m, int num) {
-        if (num == 0) {
+        if (num == 0)
             return createIdentity(m.rows_);
-        } else if (num == 1) {
+        else if (num == 1)
             return m;
-        } else if (num % 2 == 0) {  // num is even
+        else if (num % 2 == 0)
             return expHelper(m * m, num / 2);
-        } else {                    // num is odd
+        else
             return m * expHelper(m * m, (num - 1) / 2);
-        }
     }
-
-    /* NON-MEMBER FUNCTIONS
-     ********************************/
 
     Matrix operator+(const Matrix &m1, const Matrix &m2) {
         Matrix temp(m1);
-        return (temp += m2);
-    }
-
+        return (temp += m2); }
     Matrix operator-(const Matrix &m1, const Matrix &m2) {
         Matrix temp(m1);
-        return (temp -= m2);
-    }
-
+        return (temp -= m2); }
     Matrix operator*(const Matrix &m1, const Matrix &m2) {
         Matrix temp(m1);
-        return (temp *= m2);
-    }
-
+        return (temp *= m2); }
     Matrix operator*(const Matrix &m, double num) {
         Matrix temp(m);
-        return (temp *= num);
-    }
-
+        return (temp *= num); }
     Matrix operator*(double num, const Matrix &m) {
-        return (m * num);
-    }
-
+        return (m * num); }
     Matrix operator/(const Matrix &m, double num) {
         Matrix temp(m);
-        return (temp /= num);
-    }
+        return (temp /= num); }
 
     std::ostream &operator<<(std::ostream &os, const Matrix &m) {
         for (int i = 0; i < m.rows_; ++i) {
             os << m.p[i][0];
-            for (int j = 1; j < m.cols_; ++j) {
+            for (int j = 1; j < m.cols_; ++j)
                 os << " " << m.p[i][j];
-            }
-            os << std::endl;
+            os << '\n';
         }
         return os;
     }
 
     std::istream &operator>>(std::istream &is, Matrix &m) {
-        for (int i = 0; i < m.rows_; ++i) {
-            for (int j = 0; j < m.cols_; ++j) {
+        for (int i = 0; i < m.rows_; ++i)
+            for (int j = 0; j < m.cols_; ++j)
                 is >> m.p[i][j];
-            }
-        }
+
         return is;
     }
 
-    }
+}
